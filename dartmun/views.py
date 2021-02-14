1a53a6f73969162c252405b05ee3a8eb50593180
hdk23 <henry.dohyun.kim.23@dartmouth.edu>
2021-02-14 11:36:47 +0900

    Add Links to Templates
    Links added to My Committee Page and Index Page

--- dartmun/views.py
+++ dartmun/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 # Create your views here.
 def index(request):
+    """loads the index page"""
     context={}
     return render(request, 'dartmun/index.html', context)
+
+
+def my_committee(request):
+    """loads the my committee page"""
+    context = {}
+    return render(request, 'dartmun/mycommittee.html', context)
